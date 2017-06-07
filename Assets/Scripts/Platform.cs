9e02f02c7cbc5929e1d3fd908c2d0aedffdebc53
naoya kurihara <kuriharanaoya@gmail.com>
2017-06-08 00:22:40 +0900

    パネルごとの頂点数増加

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -15,18 +15,22 @@ public class Platform : MonoBehaviour
     [SerializeField]
     float TILE_WIDTH = 1.0f;
 
+    float FRAME_WIDTH = 0.2f;
+    const int vnum_per_tile = 16;
+
     public int GetPanelId(Vector3 pos)
     {
-        if( ( 0.0f > pos.x ) || (TILE_WIDTH * TILE_X_COUNT) < pos.x )
+        float with_frame_width = TILE_WIDTH + FRAME_WIDTH * 2.0f;
+        if ( ( 0.0f > pos.x ) || (with_frame_width * TILE_X_COUNT) < pos.x )
         {
             return -1;
         }
-        if( ( 0.0f > pos.z ) || (TILE_WIDTH * TILE_Z_COUNT) < pos.z )
+        if( ( 0.0f > pos.z ) || (with_frame_width * TILE_Z_COUNT) < pos.z )
         {
             return -1;
         }
-        int x = (int)(pos.x / TILE_WIDTH);
-        int z = (int)(pos.z / TILE_WIDTH);
+        int x = (int)(pos.x / with_frame_width);
+        int z = (int)(pos.z / with_frame_width);
 
         return z * TILE_X_COUNT + x;
     }
@@ -36,10 +40,25 @@ public class Platform : MonoBehaviour
         int TILE_NUM = TILE_X_COUNT * TILE_Z_COUNT;
 
         Color hilightColor = new Color(0.5f, 0.5f, 0.2f);
+        /*
         for (var i = 0; i < TILE_NUM; ++i)
         {
             colors[i * 4 + 0] = colors[i * 4 + 1] = colors[i * 4 + 2] = colors[i * 4 + 3] = (i == id) ? hilightColor : Color.black;
         }
+        */
+
+        for( int i = 0; i < colors.Length; ++i )
+        {
+            if( (vnum_per_tile * id <= i) && (vnum_per_tile * (id + 1) > i) )
+            {
+                colors[i] = hilightColor;
+            }
+            else
+            {
+                colors[i] = Color.black;
+            }
+        }
+
         mesh.colors = colors;
     }
 
@@ -50,11 +69,17 @@ public class Platform : MonoBehaviour
             return;
         }
 
-        for( int i = id * 4; i < (id + 1) * 4; ++i )
+        int[] indices = new int[4];
+        indices[0] = vnum_per_tile * id + 5;
+        indices[1] = vnum_per_tile * id + 6;
+        indices[2] = vnum_per_tile * id + 9;
+        indices[3] = vnum_per_tile * id + 10;
+
+        for ( int i = 0; i < 4; ++i )
         {
-            var p = vertices[i];
-            p.y += 0.1f;
-            vertices[i] = p;
+            var p = vertices[indices[i]];
+            p.y += 0.3f;
+            vertices[indices[i]] = p;
         }
         mesh.vertices = vertices;
 
@@ -68,41 +93,147 @@ public class Platform : MonoBehaviour
     {
         int TILE_NUM = TILE_X_COUNT * TILE_Z_COUNT;
 
-        vertices = new Vector3[TILE_NUM * 4];
+        vertices = new Vector3[TILE_NUM * vnum_per_tile];
         for( int z = 0; z < TILE_Z_COUNT; ++z )
         {
-            for( int x = 0; x < TILE_X_COUNT; ++x )
+            for (int x = 0; x < TILE_X_COUNT; ++x)
             {
                 int i = z * TILE_X_COUNT + x;
                 float y = 0.0f;
-                vertices[i * 4 + 0] = new Vector3((0 + x) * TILE_WIDTH, y, (0 + z) * TILE_WIDTH);
-                vertices[i * 4 + 1] = new Vector3((0 + x) * TILE_WIDTH, y, (1 + z) * TILE_WIDTH);
-                vertices[i * 4 + 2] = new Vector3((1 + x) * TILE_WIDTH, y, (0 + z) * TILE_WIDTH);
-                vertices[i * 4 + 3] = new Vector3((1 + x) * TILE_WIDTH, y, (1 + z) * TILE_WIDTH);
+                float with_frame_width = TILE_WIDTH + FRAME_WIDTH * 2.0f;
+
+                float x0 = x * with_frame_width;
+                float x1 = x0 + FRAME_WIDTH;
+                float x2 = x1 + TILE_WIDTH;
+                float x3 = x2 + FRAME_WIDTH;
+
+                float z0 = z * with_frame_width;
+                float z1 = z0 + FRAME_WIDTH;
+                float z2 = z1 + TILE_WIDTH;
+                float z3 = z2 + FRAME_WIDTH;
+
+                vertices[i * vnum_per_tile +  0] = new Vector3(x0, y, z0);
+                vertices[i * vnum_per_tile +  1] = new Vector3(x1, y, z0);
+                vertices[i * vnum_per_tile +  2] = new Vector3(x2, y, z0);
+                vertices[i * vnum_per_tile +  3] = new Vector3(x3, y, z0);
+
+                vertices[i * vnum_per_tile +  4] = new Vector3(x0, y, z1);
+                vertices[i * vnum_per_tile +  5] = new Vector3(x1, y, z1);
+                vertices[i * vnum_per_tile +  6] = new Vector3(x2, y, z1);
+                vertices[i * vnum_per_tile +  7] = new Vector3(x3, y, z1);
+
+                vertices[i * vnum_per_tile +  8] = new Vector3(x0, y, z2);
+                vertices[i * vnum_per_tile +  9] = new Vector3(x1, y, z2);
+                vertices[i * vnum_per_tile + 10] = new Vector3(x2, y, z2);
+                vertices[i * vnum_per_tile + 11] = new Vector3(x3, y, z2);
+
+                vertices[i * vnum_per_tile + 12] = new Vector3(x0, y, z3);
+                vertices[i * vnum_per_tile + 13] = new Vector3(x1, y, z3);
+                vertices[i * vnum_per_tile + 14] = new Vector3(x2, y, z3);
+                vertices[i * vnum_per_tile + 15] = new Vector3(x3, y, z3);
             }
         }
 
-        normals = new Vector3[TILE_NUM * 4];
-        for( var i = 0; i < TILE_NUM; ++i )
+        normals = new Vector3[TILE_NUM * vnum_per_tile];
+        for (var i = 0; i < TILE_NUM; ++i)
         {
-            normals[i * 4 + 0] = normals[i * 4 + 1] = normals[i * 4 + 2] = normals[i * 4 + 3] = Random.insideUnitSphere;
+            var normal = Random.insideUnitSphere;
+            for (int j = 0; j < vnum_per_tile; ++j)
+            {
+                normals[i * vnum_per_tile + j] = normal;
+            }
+            normals[i * vnum_per_tile + 0] = Vector3.down;
+            normals[i * vnum_per_tile + 1] = Vector3.down;
+            normals[i * vnum_per_tile + 4] = Vector3.down;
+            normals[i * vnum_per_tile + 2] = Vector3.down;
+            normals[i * vnum_per_tile + 6] = Vector3.down;
         }
 
-        colors = new Color[TILE_NUM * 4];
+        colors = new Color[TILE_NUM * vnum_per_tile];
+        var default_color = Color.black;
         for( var i = 0; i < TILE_NUM; ++i )
         {
-            colors[i * 4 + 0] = colors[i * 4 + 1] = colors[i * 4 + 2] = colors[i * 4 + 3] = Color.black;
+            for( int j = 0; j < vnum_per_tile; ++j )
+            {
+                colors[i * vnum_per_tile + j] = default_color;
+            }
         }
 
-        triangles = new int[TILE_NUM * 6];
+        const int triangle_per_tile = 54;
+        triangles = new int[TILE_NUM * triangle_per_tile];
         for (int i = 0; i < TILE_NUM; ++i)
         {
-            triangles[i * 6 + 0] = i * 4 + 0;
-            triangles[i * 6 + 1] = i * 4 + 1;
-            triangles[i * 6 + 2] = i * 4 + 2;
-            triangles[i * 6 + 3] = i * 4 + 2;
-            triangles[i * 6 + 4] = i * 4 + 1;
-            triangles[i * 6 + 5] = i * 4 + 3;
+            triangles[i * triangle_per_tile + 0] = i * vnum_per_tile + 0;
+            triangles[i * triangle_per_tile + 1] = i * vnum_per_tile + 4;
+            triangles[i * triangle_per_tile + 2] = i * vnum_per_tile + 1;
+
+            triangles[i * triangle_per_tile + 3] = i * vnum_per_tile + 1;
+            triangles[i * triangle_per_tile + 4] = i * vnum_per_tile + 4;
+            triangles[i * triangle_per_tile + 5] = i * vnum_per_tile + 5;
+
+            triangles[i * triangle_per_tile + 6] = i * vnum_per_tile + 1;
+            triangles[i * triangle_per_tile + 7] = i * vnum_per_tile + 5;
+            triangles[i * triangle_per_tile + 8] = i * vnum_per_tile + 2;
+
+            triangles[i * triangle_per_tile + 9] = i * vnum_per_tile + 2;
+            triangles[i * triangle_per_tile +10] = i * vnum_per_tile + 5;
+            triangles[i * triangle_per_tile +11] = i * vnum_per_tile + 6;
+
+            triangles[i * triangle_per_tile + 12] = i * vnum_per_tile + 2;
+            triangles[i * triangle_per_tile + 13] = i * vnum_per_tile + 6;
+            triangles[i * triangle_per_tile + 14] = i * vnum_per_tile + 3;
+
+            triangles[i * triangle_per_tile + 15] = i * vnum_per_tile + 3;
+            triangles[i * triangle_per_tile + 16] = i * vnum_per_tile + 6;
+            triangles[i * triangle_per_tile + 17] = i * vnum_per_tile + 7;
+
+            triangles[i * triangle_per_tile + 18] = i * vnum_per_tile + 4;
+            triangles[i * triangle_per_tile + 19] = i * vnum_per_tile + 8;
+            triangles[i * triangle_per_tile + 20] = i * vnum_per_tile + 5;
+
+            triangles[i * triangle_per_tile + 21] = i * vnum_per_tile + 5;
+            triangles[i * triangle_per_tile + 22] = i * vnum_per_tile + 8;
+            triangles[i * triangle_per_tile + 23] = i * vnum_per_tile + 9;
+
+            triangles[i * triangle_per_tile + 24] = i * vnum_per_tile + 5;
+            triangles[i * triangle_per_tile + 25] = i * vnum_per_tile + 9;
+            triangles[i * triangle_per_tile + 26] = i * vnum_per_tile + 6;
+
+            triangles[i * triangle_per_tile + 27] = i * vnum_per_tile + 6;
+            triangles[i * triangle_per_tile + 28] = i * vnum_per_tile + 9;
+            triangles[i * triangle_per_tile + 29] = i * vnum_per_tile + 10;
+
+            triangles[i * triangle_per_tile + 30] = i * vnum_per_tile + 6;
+            triangles[i * triangle_per_tile + 31] = i * vnum_per_tile + 10;
+            triangles[i * triangle_per_tile + 32] = i * vnum_per_tile + 7;
+
+            triangles[i * triangle_per_tile + 33] = i * vnum_per_tile + 7;
+            triangles[i * triangle_per_tile + 34] = i * vnum_per_tile + 10;
+            triangles[i * triangle_per_tile + 35] = i * vnum_per_tile + 11;
+
+            triangles[i * triangle_per_tile + 36] = i * vnum_per_tile + 8;
+            triangles[i * triangle_per_tile + 37] = i * vnum_per_tile + 12;
+            triangles[i * triangle_per_tile + 38] = i * vnum_per_tile + 9;
+
+            triangles[i * triangle_per_tile + 39] = i * vnum_per_tile + 9;
+            triangles[i * triangle_per_tile + 40] = i * vnum_per_tile + 12;
+            triangles[i * triangle_per_tile + 41] = i * vnum_per_tile + 13;
+
+            triangles[i * triangle_per_tile + 42] = i * vnum_per_tile + 9;
+            triangles[i * triangle_per_tile + 43] = i * vnum_per_tile + 13;
+            triangles[i * triangle_per_tile + 44] = i * vnum_per_tile + 10;
+
+            triangles[i * triangle_per_tile + 45] = i * vnum_per_tile + 10;
+            triangles[i * triangle_per_tile + 46] = i * vnum_per_tile + 13;
+            triangles[i * triangle_per_tile + 47] = i * vnum_per_tile + 14;
+
+            triangles[i * triangle_per_tile + 48] = i * vnum_per_tile + 10;
+            triangles[i * triangle_per_tile + 49] = i * vnum_per_tile + 14;
+            triangles[i * triangle_per_tile + 50] = i * vnum_per_tile + 11;
+
+            triangles[i * triangle_per_tile + 51] = i * vnum_per_tile + 11;
+            triangles[i * triangle_per_tile + 52] = i * vnum_per_tile + 14;
+            triangles[i * triangle_per_tile + 53] = i * vnum_per_tile + 15;
         }
 
 
